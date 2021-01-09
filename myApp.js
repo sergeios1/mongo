c9a3f4394aead16f08202b583917ecfd9b7a9996
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 12:04:51 -0800

    a

--- myApp.js
+++ myApp.js
@@ -3,13 +3,8 @@ var express = require('express');
 var app = express();
 const mongoose = require('mongoose');
 
-/** 1) Install & Set up mongoose */
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
-/** 3) Create and Save a Person */
 
 
 
