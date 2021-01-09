d9e7341c5dca05bc367659e23a17eaa376612e44
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 13:43:33 -0800

    a

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let personSchema = mongoose.Schema({
-  name: String,
+  name:{type: String, required: true},
   age: Number,
-  favoiteFoods: [String]
+  favortieFoods: [String]
 });
 
 let Person = mongoose.model("Person", personSchema);
