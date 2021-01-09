51f68f16a58c9122bee43603bc6ed37f655435ef
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 12:01:43 -0800

    a

--- myApp.js
+++ myApp.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 
 /** 1) Install & Set up mongoose */
 
-const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
