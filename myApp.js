7421758a36ee45e03f052ee795491ede827772b0
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 16:51:04 -0800

    goldengod

--- myApp.js
+++ myApp.js
@@ -79,7 +79,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({_id: personId},(err,data) => {
+    if(err)return console.error(err);
+    done(null, data);
+  })
 };
 
 const removeManyPeople = (done) => {
