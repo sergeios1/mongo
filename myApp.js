9815202107531755e8473ba0e8a00d9699eaf569
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 16:25:45 -0800

    work

--- myApp.js
+++ myApp.js
@@ -69,12 +69,15 @@ const findEditThenSave = (personId, done) => {
   })
 };
 
+
+
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-  Person.findOneAndUpdate({name: personName}, {age: agetoSet}, {new: true},(err,updatedDoc) => {
+
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
     if(err) return console.log(err);
     done(null, updatedDoc);
-  }) 
+  })
 };
 
 const removeById = (personId, done) => {
