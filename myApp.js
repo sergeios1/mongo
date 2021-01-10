cf41e09b60b992d5bb09e2a104c94923782f92a1
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 17:07:48 -0800

    championofthesun

--- myApp.js
+++ myApp.js
@@ -97,7 +97,10 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({name: 1, age: 0, favoriteFoods: 1}).exec((err,data) => {
+    if(err)return console.error(err);
+    done(null, data);
+  })
 };
 
 /** **Well Done !!**
