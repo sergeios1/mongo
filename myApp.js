0cc41a64495b544cee85e3b28f97dffe6022c468
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 17:10:43 -0800

    asdfasdfasdfsd

--- myApp.js
+++ myApp.js
@@ -97,7 +97,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({name: 1, age: 0, favoriteFoods: 1}).exec((err,data) => {
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age: 0}).exec((err,data) => {
     if(err)return console.error(err);
     done(null, data);
   })
