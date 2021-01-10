34663bbfd4e0976aaef143b0dc66def72705feac
sergios1 <sergetnazaretyan@gmail.com>
2021-01-09 16:54:39 -0800

    fighterofthenightman

--- myApp.js
+++ myApp.js
@@ -88,7 +88,10 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, data) => {
+    if(err)return console.error(err);
+    done(null, data);
+  })
 };
 
 const queryChain = (done) => {
